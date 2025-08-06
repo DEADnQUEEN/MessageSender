d39c1cfa261c73396d950e59e195c1d369f46111
DEADnQUEEN <aleks20aleks0228@gmail.com@gmail.com>
2025-08-06 18:09:45 +0300

    config for api whatsapp

---
+++ messageSender/WhatsAppBusinessApiSender/config.py
@@ -0,0 +1,8 @@
+import json
+import os
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
+
+with open(os.path.join(os.getcwd(), "config", "endpoints.json"), "r") as config_file:
+    ENDPOINTS = json.load(config_file)
